0217cb1c9fc6c3235299bdff1fa394c4884f544b
Thales <thalesreis.es@gmail.com>
2025-08-28 14:20:29 -0300

    Finalização do Projeto

--- Construindo_paginas_dinamicas/index.js
+++ Construindo_paginas_dinamicas/index.js
@@ -1,53 +1,18 @@
-const inputItem = document.getElementById("input-item")
-let contador = 0;
-const listaDeCompras = document.getElementById("lista-de-compras")
+import { criarItemDaLista } from "./scripts/CriarItemDaLista.js";
+import  verificarListaVazia  from "./scripts/verificarListaVazia.js";
 
 const botaoAdicionar = document.getElementById("adicionar-item")
+const listaDeCompras = document.getElementById("lista-de-compras")
+
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    console.log(inputItem.value)
-    if(inputItem.value === ""){
-        alert("Você se esqueceu de preencher!!")
-        return
-    }
-
-    const itemDaLista = document.createElement("li")
-    const containerItemDaLista = document.createElement("div")
-    containerItemDaLista.classList.add("lista-item-container")
-    const inputCheckbox = document.createElement("input");
-    inputCheckbox.type = "checkbox";
-    inputCheckbox.id = "checkbox-" + contador++;
-    const nomeItem = document.createElement('p')
-    nomeItem.innerText = inputItem.value;
-
-    inputCheckbox.addEventListener("click", function(){
-        if(inputCheckbox.checked){
-            nomeItem.style.textDecoration = 'line-through';
-        }else{
-            nomeItem.style.textDecoration = 'none';
-        }
-    })
-
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-    itemDaLista.appendChild(containerItemDaLista)
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-br", {weekday: "long"})
-    const data = new Date().toLocaleDateString("pt-br")
-    const hora = new Date().toLocaleString("pt-br", {hour: "numeric", minute: "numeric"})
-
-    const dataCompleta = `${diaDaSemana} (${data} ás ${hora})`
-    const itemData = document.createElement("p")
-    itemData.innerText = dataCompleta;
-    itemData.classList.add("texto-data")
-    
-    itemDaLista.appendChild(itemData)
-    
-    
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista)
-
+    verificarListaVazia(listaDeCompras);
 })
 
 
+
+verificarListaVazia(listaDeCompras);
 
